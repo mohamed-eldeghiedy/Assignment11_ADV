b7ec42df8aa2d56f698d0b751cdf94a2b9622d74
mohamed-eldeghiedy <eldegedym@gmail.com>
2025-08-03 04:41:23 +0300

    Add project files.

---
+++ Assignment11_ADV/Class1.cs
@@ -0,0 +1,60 @@
+﻿using Assignment11_ADV;
+using System;
+using System.Collections.Generic;
+
+
+namespace Assignment11_ADV
+{
+
+
+
+    public delegate string BookDelegate(Book b);
+
+
+    public class Book
+    {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string[] Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public decimal Price { get; set; }
+
+        public Book(string _ISBN, string _Title, string[] _Authors, DateTime _PublicationDate, decimal _Price)
+        {
+            ISBN = _ISBN;
+            Title = _Title;
+            Authors = _Authors;
+            PublicationDate = _PublicationDate;
+            Price = _Price;
+        }
+
+        public override string ToString()
+        {
+            return $"ISBN: {ISBN}\n" +
+                   $"Title: {Title}\n" +
+                   $"Authors: {string.Join(", ", Authors)}\n" +
+                   $"Publication Date: {PublicationDate:yyyy-MM-dd}\n" +
+                   $"Price: {Price:C}";
+        }
+    }
+
+    public class BookFunctions
+    {
+        public static string GetTitle(Book B) => B.Title;
+        public static string GetAuthors(Book B) => string.Join(", ", B.Authors);
+        public static string GetPrice(Book B) => B.Price.ToString("C");
+    }
+
+
+    public class LibraryEngine
+    {
+
+        public static void ProcessBooks(List<Book> bList, Func<Book, string> fPtr)
+        {
+            foreach (Book B in bList)
+            {
+                Console.WriteLine(fPtr(B));
+            }
+        }
+    }
+}
---
+++ Assignment11_ADV/Program.cs
@@ -0,0 +1,29 @@
+﻿using static Assignment11_ADV.BookFunctions;
+
+namespace Assignment11_ADV
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            List<Book> bookList = new List<Book>
+            {
+                new Book("12345", "C# Basics", new string[]{"Mohamed Ahmed"}, new DateTime(2020, 7, 1), 200m),
+                new Book("67890", "Advanced C#", new string[]{"yasmin Ahmed"}, new DateTime(2022, 4, 19), 300m)
+            };
+
+            BookDelegate myDel = new BookDelegate(BookFunctions.GetTitle);
+            LibraryEngine.ProcessBooks(bookList, new Func<Book, string>(myDel));
+
+
+            LibraryEngine.ProcessBooks(bookList, BookFunctions.GetAuthors);
+
+
+            LibraryEngine.ProcessBooks(bookList, delegate (Book b) { return b.ISBN; });
+
+
+            LibraryEngine.ProcessBooks(bookList, b => b.PublicationDate.ToString("yyyy-MM-dd"));
+        }
+    }
+}
